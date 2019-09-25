ec56ffe65167ed0ea81aedaf2c1b86cb34549053
“leeon” <499053596@qq.com>
2019-09-25 20:49:26 +0800

    初始项目代码

---
+++ src/微服务/网关服务/Galaxy.Taurus.GatewayAPI/Controllers/HealthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Galaxy.Taurus.GatewayAPI.Controllers
+{
+    [Route("gatewayAPI/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get() => Ok("网关服务正常!");
+    }
+}
---
+++ src/微服务/网关服务/Galaxy.Taurus.GatewayAPI/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.Configuration;
+using System.Net;
+
+namespace Galaxy.Taurus.GatewayAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, builder) => {
+                    builder
+                        .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                        .AddJsonFile("ocelotConfig.json");
+                })
+                .UseKestrel(SetHost)
+                .UseStartup<Startup>();
+
+        /// <summary>
+        /// 配置Kestrel
+        /// </summary>
+        private static void SetHost(KestrelServerOptions options)
+        {
+            var address = IPAddress.Parse("0.0.0.0");
+            options.Listen(address, 8880);
+            options.UseSystemd();
+        }
+    }
+}
